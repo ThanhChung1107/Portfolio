f0f02411a8ec59b22f52cacec941443f97d36eff
ThanhChung1107 <lethanhchung1107@gmail.com>
2025-09-07 16:55:35 +0700

    portfolio

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
-import { Navbar } from "../components/Navbar";
+import { Navbar } from "../components/NavBar";
 import { ThemeToggle } from "../components/ThemeToggle";
-import { StarBackground } from "@/components/StarBackground";
+import { StarBackground } from "../components/StarBackground";
 import { HeroSection } from "../components/HeroSection";
 import { AboutSection } from "../components/AboutSection";
 import { SkillsSection } from "../components/SkillsSection";
